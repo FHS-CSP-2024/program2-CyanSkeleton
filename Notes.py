ff2eb6bccce84f4297102e60f0c30926b76629d1
CyanSkeleton <155357938+CyanSkeleton@users.noreply.github.com>
2024-09-04 10:38:37 -0700

    Update Notes.py

--- Notes.py
+++ Notes.py
@@ -1,1 +1,12 @@
+name = input("What's your name?\n\t")
+age = input("How old are you?\n\t")
 
+# got bored
+for i in range(5):
+    print("hello " + name)
+if int(age) < 8:
+    print("Go read a book or something")
+elif int(age) > 80:
+    print("Damn, thats unexpected")
+else:
+    print("Alright")
