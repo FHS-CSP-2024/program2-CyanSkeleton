66cb10c209664e0a3c80732fe9f75a79b1e47c2a
CyanSkeleton <155357938+CyanSkeleton@users.noreply.github.com>
2024-09-04 10:37:59 -0700

    Update Program2.py
    codespaces commiting taking too long

--- Program2.py
+++ Program2.py
@@ -50,17 +50,21 @@
 #Please write a script that: 
 # - Asks for the user's name and then prints it twice, on two consecutive lines.
 
+name = input("What is your name?\n\t")
+print(name)
+print(name)
 
 
-
-
+print()
 ## Problem 2 ##
 #Please write a script that: 
 # - Asks for the user's name
 # - Prints it out twice on a single line so that there is an exclamation mark at the beginning of the line, 
 # - another between the two names and a third one at the end of the line.
+name = input("What is your name?\n\t")
+print("!" + name + "!" + name + "!")
 
-
+print()
 ## Problem 3 ##
 #Please write a script that: 
 # - Asks for the user's name and address. 
@@ -70,13 +74,25 @@
 #   - Last name: Sanders
 #   - Street address: 91 Station Road
 #   - City and postal code: Folsom CA, 95630
+name = input("What is your first and last name?\n\t")
+nameArray = name.split()
+streetAddress = input("What is your street address?\n\t")
+capc = input("What is your city and postal code?\n\t")
+print("First name: " + nameArray[0])
+print("Last name: " + nameArray[1])
+print("Street Address: " + streetAddress)
+print("City and postal code: " + capc)
 
+print()
 ## Problem 4 ##
 #Please write a script that: 
 # - Asks for 3 words 
 # - Puts the words together with dashes and prints that out
+threeWords = input("Give me 3 words?\n\t")
+wordArray = threeWords.split()
+print(wordArray[0] + "-" + wordArray[1] + "-" + wordArray[2])
 
-
+print()
 ## Problem 5 ##
 #Please write a script that: 
 # - Asks for a name and a year
@@ -88,3 +104,11 @@
 #Mary is a valiant knight, born in the year 1572. 
 #One morning Mary woke up to an awful racket: a dragon was approaching the village. 
 #Only Mary could save the village's residents.
+name = input("What is your name\n\t")
+print("this is a completely original game by the big game studio bugthesda")
+print(name + " is a guy being executed by the imperial empire")
+print(name + " escapes and kills a dragon and learns that they have magic powers")
+print("then " + name + " finds a secret organization dedicated to killing dragons")
+print("they have to stop a dragon from reviving dragons and causing the end of the world")
+print(name + " meets with a wise peaceful dragon that teaches them how to beat the evil dragon")
+print(name + " then kills the evil dragon and saves the world")
